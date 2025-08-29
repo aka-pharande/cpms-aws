0054f84f20aa4182b7ed770bc1024aa937bc353f
Aakanksha <aka.pharande@gmail.com>
2025-08-28 20:49:23 -0400

    Fix proto

--- terraform/main.tf
+++ terraform/main.tf
@@ -90,9 +90,9 @@ resource "aws_security_group" "rds" {
   }
 
   ingress {
-    from_port        = 0
-    to_port          = 0
-    protocol         = "mysql/aurora"
+    from_port   = 3306
+    to_port     = 3306
+    protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
 
